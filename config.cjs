c07fd62daaf12eeaa2d842dd6b867ab6fa16426d
jeandaniel0010 <jeandaniel0010@gmail.com>
2025-09-08 22:29:51 +0000

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,13 +3,12 @@ require("dotenv").config();
 
 const config = {
   // 🧠 Session & Identity
-  SESSION_ID: process.env.SESSION_ID || "NECTOR;;;FtQXlABI#PS9r9TFh5n55o4tNExBJVjASU6lWdR89ImV2vd2VaaE",
+  SESSION_ID: process.env.SESSION_ID || "NECTOR;;;94I2QLrY#_YzneAqXdy92jr6GVR4nuMwc1qpJGixX1M1X3BK1OfE",
   PREFIX: process.env.PREFIX || '.',
   BOT_NAME: process.env.BOT_NAME || "THE-HUB-BOT",
   BOT: process.env.BOT || "hello 👋",
   NEW_CMD: process.env.NEW_CMD || "ᴀᴅᴅᴠᴀʀ\n│ sᴜᴅᴏ\n| nector",
-  CAPTION: process.env.CAPTION || "ᴘᴏᴡᴇʀᴇᴅ by nector",
-
+  CAPTION: process.env.CAPTION || "false",
   // 🤖 GPT/AI API KEYS
   GPT_API_KEY: process.env.GPT_API_KEY || "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", // Groq key
   GROQ_API_KEY: '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',
@@ -19,11 +18,11 @@ const config = {
  
   // 🔧 Auto features
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN === 'true',
-  AUTO_BIO: process.env.AUTO_BIO === 'true',
-  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT === 'true',
-  AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS === 'true',
-  AUTO_STICKER: process.env.AUTO_STICKER === 'true',
-  AUTO_READ: process.env.AUTO_READ === 'true',
+  AUTO_BIO: process.env.AUTO_BIO === 'false',
+  AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT === 'false',
+  AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS === 'false',
+  AUTO_STICKER: process.env.AUTO_STICKER === 'false',
+  AUTO_READ: process.env.AUTO_READ === 'false',
   AUTO_TYPING: process.env.AUTO_TYPING === 'true',
   AUTO_RECORDING: process.env.AUTO_RECORDING === 'true',
   AUTO_REACT: process.env.AUTO_REACT === 'true',
@@ -31,9 +30,9 @@ const config = {
   AUTO_BLOCK: process.env.AUTO_BLOCK !== 'false', // true by default
 
   // 📦 Extra Features
-  ANTI_LEFT: process.env.ANTI_LEFT === 'true',
-  ANTILINK: process.env.ANTILINK === 'true',
-  ANTI_DELETE: process.env.ANTI_DELETE === 'true',
+  ANTI_LEFT: process.env.ANTI_LEFT === 'false',
+  ANTILINK: process.env.ANTILINK === 'false',
+  ANTI_DELETE: process.env.ANTI_DELETE === 'false',
   CHAT_BOT: process.env.CHAT_BOT === 'true',
   CHAT_BOT_MODE: process.env.CHAT_BOT_MODE || "public",
   LYDEA: process.env.LYDEA === 'true',
@@ -43,16 +42,16 @@ const config = {
 
   // 🛠 Other
   MODE: process.env.MODE || "public",
-  DELETED_MESSAGES_CHAT_ID: process.env.DELETED_MESSAGES_CHAT_ID || "254725474072@s.whatsapp.net",
+  DELETED_MESSAGES_CHAT_ID: process.env.DELETED_MESSAGES_CHAT_ID || "2250500694652@s.whatsapp.net",
 
   // 👑 Owner & Permissions
   OWNER_NAME: process.env.OWNER_NAME || "❤️ⓃⒺCⓉOR🍯",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254725474072",
-  SUDO_NUMBER: process.env.SUDO_NUMBER || "254725474072",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "2250500694652",
+  SUDO_NUMBER: process.env.SUDO_NUMBER || "2250500694652",
 
   // 💚 Auto react emoji
-  AUTOLIKE_EMOJI: process.env.AUTOLIKE_EMOJI || '💚',
-  STATUS_READ_MSG: process.env.STATUS_READ_MSG || 'Status Viewed by THE-HUB-BOT',
+  AUTOLIKE_EMOJI: process.env.AUTOLIKE_EMOJI || 'false',
+  STATUS_READ_MSG: process.env.STATUS_READ_MSG || 'false',
   WELCOME: process.env.WELCOME === 'true',
 };
 
